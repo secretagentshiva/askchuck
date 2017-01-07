3847b8a24a76eac78b29652435f805f933418c31
Shiva Rajaraman <shiva@AirShiva.local>
2017-01-07 17:57:33 -0500

    Auto layout update

--- AskChuck/ViewController.swift
+++ AskChuck/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textPassword: UITextField!
+    @IBOutlet weak var labelWelcome: UILabel!
+    var intLoginAttempts: UInt! = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        if textPassword.text == "poo" {
+            labelWelcome.text = "I'M SO PROUD OF YOU!"
+            intLoginAttempts = 0
+        }
+            
+        else {
+            intLoginAttempts = intLoginAttempts + 1
+            
+            if intLoginAttempts > 1 {
+                labelWelcome.text = "POO ON YOU! HELP POO BROTHER!"
+            }
+                
+            else {
+                labelWelcome.text = "THAT'S REALLY CREEPY!"
+            }
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +48,11 @@ class ViewController: UIViewController {
 
 
 }
+
+// Save for "I'm feeling chucky" RANDOM selection
+// var countAssets: UInt32 = 10 // will need to populate with lookup of available assets
+// let selectionSurprise = arc4random_uniform(countAssets+1)
+
+
+
 
